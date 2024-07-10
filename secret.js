76233b10e2daed433481e0cf6778d8daf4c69858
Dan Toms <157254317+pynmash@users.noreply.github.com>
2024-07-10 11:31:44 +0100

    Entire project

---
+++ secret.js
@@ -0,0 +1,13 @@
+alert("Hello Amelie! This is Daddy talking to you from beyond the webpage!");
+alert(
+  "You've found my super secret messages! While you're here, I may as well spill the beans",
+);
+
+let response = prompt(
+  "Did you know that clown farts........(type an adjective or describing sentence and press enter)",
+  "smell of bog",
+);
+
+alert(
+  `Clown farts are REALLLYYYY FUNNY! They're so loud and ${response}. Your socks will fall off if you sniff one!`,
+);
